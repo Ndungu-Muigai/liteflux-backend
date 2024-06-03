09ee946019356b91006fe61585fa91d7b8db0cf9
Samuel Muigai <ndungu.muigai01@gmail.com>
2024-06-03 14:59:56 +0300

    Saving the images locally before sending them to the bucket

--- api/app.py
+++ api/app.py
@@ -131,9 +131,10 @@ def add_product():
 
         # Uploading the image to the S3 bucket
         try:
-            with open(image_path, "rb") as data:
-                s3_client.put_object(Bucket=S3_BUCKET_NAME, Key=unique_image_name, Body=data)
-                image_urls.append({"image_name": unique_image_name, "image_url": f"{S3_BASE_URL}{unique_image_name}"})
+            # with open(image,"rb") as data:
+            s3_client.Bucket(S3_BUCKET_NAME).put_object(Key=unique_image_name,Body=image_path)
+            # s3_client.upload_fileobj(image_path, S3_BUCKET_NAME, unique_image_name)
+            image_urls.append({"image_name": unique_image_name, "image_url": f"{S3_BASE_URL}{unique_image_name}"})
         except Exception as e:
             return make_response(jsonify({"error": f"Error uploading image to Digital Ocean: {e}"}), 404)
 
