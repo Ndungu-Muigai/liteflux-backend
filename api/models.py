6813b8f2e8b130148026cff13c5640f8451b1272
Samuel Muigai <ndungu.muigai01@gmail.com>
2024-06-05 20:53:50 +0300

    Modified the order product and order schema

--- api/models.py
+++ api/models.py
@@ -44,7 +44,7 @@ class Order(db.Model):
 class OrderProduct(db.Model):
     __tablename__ = "order_products"
     id = db.Column(db.Integer, primary_key=True)
-    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
+    product_id = db.Column(db.Integer, nullable=False)
     order_id = db.Column(db.Integer, db.ForeignKey('orders.id'), nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
     
