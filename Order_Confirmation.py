200ebe52cab41b59415899b43f214f0c89f2ec91
Samuel Muigai <107753424+Ndungu-Muigai@users.noreply.github.com>
2024-06-06 12:12:45 +0300

    Working on adding the products to the order confirmation email

--- Order_Confirmation.py
+++ Order_Confirmation.py
@@ -9,20 +9,38 @@ configuration.api_key["api-key"] = os.environ.get("SENDINBLUE_API_KEY")
 api_instance=sib_api_v3_sdk.TransactionalEmailsApi(sib_api_v3_sdk.ApiClient(configuration))
 
 def confirm_order(first_name, last_name, email, order_id):
-    products_response=requests.get("https://products.litefluxent.com/products")
-    products=products_response.json()
-
-    order_products=[]
-
+    #Fetching the order from the backend 
     order_response=requests.get(f"https://api.litefluxent.com/client/orders/{order_id}")
     order=order_response.json()
+
+    #Getting the order's products
+    order_products=order.order_products
+
+    #Mapping through the order products
+    for order_product in order_products:
+        print(order_product.id)
+        #Fetching the product details from the products API
+        product_response=requests.get(f"https://products.litefluxent.com/products/{order_product.id}")
+        product=product_response.json()
+
     subject="Order confirmation"
     sender={"name": app.app.config["SENDER_NAME"], "email": app.app.config["SENDER_EMAIL"]}
     email_content=f"""
     <p>Dear {first_name} {last_name},</p>
     <p>Your order with ID {order_id} has been successfully created</p>
-    {order}
+    {product}
     <p>We will inform you once the product is ready for delivery</p>
+
+    <table>
+        <thead>
+            <tr>
+                <th colspan='3'>Product summary</th>
+            </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+    </table>
     <b>NB:This is an system generated email. Please DO NOT reply to this email thread.</b>
     """
 
